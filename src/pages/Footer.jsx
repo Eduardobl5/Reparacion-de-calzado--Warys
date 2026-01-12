f1e60e8676c1262b27e85083feecfdaa5aeb89d1
Eduardo Blanco <eduardoblanco58@gmail.com>
2026-01-12 16:42:26 -0600

    cambio en el footer

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <footer className="bg-gray-900 text-gray-400 text-sm text-center py-4">
       
-      © {new Date().getFullYear()} Eduardo Blanco. Todos los derechos reservados.
+      © {new Date().getFullYear()} Reparación de clazado y articulos de piel Warys. Todos los derechos reservados.
     </footer>
   )
 }
